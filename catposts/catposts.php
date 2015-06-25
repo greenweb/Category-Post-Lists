2634032b03743f9c307307c215244d8ec5b8f176
Rew Rixom <rew@rixom.org>
2015-06-25 12:35:34 -0400

    New Attributes
    Signed-off-by: Rew Rixom <rew@rixom.org>

--- catposts/catposts.php
+++ catposts/catposts.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Category Post Lists
 Description: Use the following ShortCode: <code>[catposts]</code> shortCode has the following attributes <code>orderby='name' OR 'id' OR 'count' OR 'name' OR 'slug' hide_empty=1 OR 0 order='DESC' OR 'ASC' exclude='0' OR '1,20,39'</code> eg: <code>[catposts orderby='id' exclude='1,20,39']</code>
-Plugin URI: http://www.beforesite.com/
+Plugin URI: http://www.beforesite.com/docs/category-post-lists-shortcode/
 Author: Rew Rixom
 Author URI: http://www.greenvilleweb.com/
 Version: 1.0
@@ -12,7 +12,7 @@ Domain Path: /
 */
 
 /*
-  Copyright (C) 2015  Author  (email : rew@rixom.org)
+  Copyright (C) 2015  Rew Rixom  (email : rew@rixom.org)
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2, as
@@ -32,11 +32,13 @@ function cp_shortcode( $atts ) {
   // Attributes
   extract( shortcode_atts(
       array(
-        'orderby'    => 'name', #id, count, name, slug,
-        'hide_empty' =>  1, # 0
-        'order'      => 'DESC', #'ASC'
-        'exclude'    => '0', #'1,20,39'
-        'include'    => ''
+        'orderby'       => 'name', #id, count, name, slug,
+        'hide_empty'    =>  1, # 0
+        'order'         => 'DESC', #'ASC'
+        'exclude'       => '0', #'1,20,39'
+        'include'       => '',
+        'post_orderby'  => 'date', 
+        'post_order'    => 'DESC', #'ASC'
       ), $atts )
   );
   
@@ -50,10 +52,10 @@ function cp_shortcode( $atts ) {
 
   $return_html ='<ul class="catposts">';
   foreach ($categories as $key => $value) {
-    $return_html  .= '<li><h3><a href="'.get_category_link( $value->term_id ).'">';
+    $return_html  .= '<li><h3 class="cat-titles"><a href="'.get_category_link( $value->term_id ).'">';
       $return_html .= $value->name;
     $return_html .= '</h3>';
-    $return_html .= cp_get_posts( $value->term_id );
+    $return_html .= cp_get_posts( $value->term_id, $post_orderby, $post_order );
     $return_html .= '</li>';
   }
   $return_html .='</ul>';
@@ -62,22 +64,20 @@ function cp_shortcode( $atts ) {
 
 add_shortcode( 'catposts', 'cp_shortcode' );
 
-function cp_get_posts($id){
+function cp_get_posts($id, $post_orderby='date', $post_order='DESC' ){
     $args = array(
       'posts_per_page'   => -1,
-      'cat'         => $id,
-      'orderby'          => 'date',
-      'order'            => 'DESC',
+      'cat'              => $id,
+      'orderby'          => $post_orderby,
+      'order'            => $post_order,
       'post_type'        => 'post',
-      // 'post_status'      => 'publish',
-      // 'suppress_filters' => true
   );
   $posts_array = new WP_Query( $args );
 
   $posts_title = '<ul>';
   while ( $posts_array->have_posts() ) {
     $posts_array->the_post();
-    $posts_title .= '<li><a href="';
+    $posts_title .= '<li class="post-titles"><a href="';
       $posts_title .= get_the_permalink();
     $posts_title .= '">';
       $posts_title .= get_the_title();
@@ -88,6 +88,7 @@ function cp_get_posts($id){
   wp_reset_postdata();
   return $posts_title;
 }
+
 
 
 // EOF
